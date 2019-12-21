392b30f8b93781590450775a9a4268a690a7401d
Patel, Chirag (cp121p) <cp121p@att.com>
2019-12-20 20:57:22 -0500

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
+
 const app = express();
 
 // Connect Database
@@ -17,7 +18,7 @@ app.use('/api/contacts', require('./routes/contacts'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
